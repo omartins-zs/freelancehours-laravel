c77cae7a6f3224a194151d9dd16fc1ddfddfa42e
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-09 12:05:40 -0300

    Ajustando a Migration para Estrutura do Project

--- database/migrations/2024_10_09_144915_create_projects_table.php
+++ database/migrations/2024_10_09_144915_create_projects_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->datetime('ends_at');
+            $table->string('status')->default('open');
+            $table->json('tech_stack')->default(0);
+            $table->foreignIdFor(User::class, 'created_by')->constrained();
             $table->timestamps();
         });
     }
