79a56eae3efcd0c02a443a34482ccc4120cad5a6
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-09 10:07:43 -0300

    Aprendendo a Usar Livewire, Layout e Component

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class WelcomeController extends Controller
 {
@@ -12,21 +10,6 @@ class WelcomeController extends Controller
     public function __invoke(Request $request)
     {
 
-        $user = User::find(1);
-
-        // $user = User::query()->create([
-        //     'name' => 'Gabriel Matheus',
-        //     'email' => 'gabriel@email.com',
-        //     'password' => 'password',
-        // ]);
-
-        //  Exemplo 1
-        $user->email_verified_at = now();
-        $user->save();
-
-        Auth::login($user);
-        Auth::logout();
-
-        return view('teste.jorge');
+        return view('welcome');
     }
 }
