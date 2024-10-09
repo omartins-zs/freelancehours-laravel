53e697186bf14cdd4d5795235262ced034f6e802
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-08 22:14:40 -0300

    Retornando a View Welcome

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,11 +6,9 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
+
     public function __invoke(Request $request)
     {
-      return self::class;
+        return view('welcome');
     }
 }
