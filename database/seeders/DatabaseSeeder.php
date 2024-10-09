27bf5f9a6d3af416077deb9c52401fd7d5cfadb2
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-09 11:41:05 -0300

    Usando o Seed para Popular o Banco de Dados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->count(20)->create();
+
+        // Cria um usuário específico
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
