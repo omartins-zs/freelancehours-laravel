00765d9de39f3e545aa768f16ca9111c5676415f
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-08 23:23:08 -0300

    Fazendo create de User

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
@@ -9,6 +10,17 @@ class WelcomeController extends Controller
 
     public function __invoke(Request $request)
     {
+        $user = User::query()->create([
+            'name' => 'Gabriel Matheus',
+            'email' => 'gabriel@email.com',
+            'password' => 'password',
+        ]);
+
+        var_dump($user);
+        dump($user);
+        // Dump and Die
+        dd($user);
+
         return view('teste.jorge');
     }
 }
