5665b466b911856275fa08aec4d0a350fc9f71fe
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-10 15:29:58 -0300

    Add Mensagens de Retorno para o Status do Projeto

--- app/ProjectStatus.php
+++ app/ProjectStatus.php
@@ -2,4 +2,26 @@
 
 namespace App;
 
-enum ProjectStatus: string {}
+enum ProjectStatus: string
+{
+    case Open = 'open';
+    case Closed = 'closed';
+
+    public function label(): string
+    {
+        // Opção 1
+        if ($this == self::Open) {
+            return 'Aceitando propostas';
+        }
+
+        if ($this == self::Closed) {
+            return 'Encerrado';
+        }
+
+        // Opção 2
+        return match ($this) {
+            self::Open => 'Aceitando propostas',
+            self::Closed => 'Encerrando',
+        };
+    }
+}
