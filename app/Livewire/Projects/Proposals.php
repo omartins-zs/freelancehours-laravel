433572d38b80a091bea068fe83157c341538f956
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-10 16:50:43 -0300

    Add o Project no Proposals(Propostas)

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -2,10 +2,13 @@
 
 namespace App\Livewire\Projects;
 
+use App\Models\Project;
 use Livewire\Component;
 
 class Proposals extends Component
 {
+    public Project $project;
+
     public function render()
     {
         return view('livewire.projects.proposals');
