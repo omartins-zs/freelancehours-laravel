ee09834f8d55423069e9cc7a9eb385cb595907fd
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-09 22:15:32 -0300

    Criando Rotas para Projects e Project ID

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', WelcomeController::class);
+Route::view('/', 'projects.index');
+
+Route::view('/project/{project}', 'projects.show')->name('projects.show');
