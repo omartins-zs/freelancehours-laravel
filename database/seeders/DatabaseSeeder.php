1d3471cc068e26824400ec2554234bb4b3e843e8
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-09 21:43:45 -0300

    Add Proposal com project_id no Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,7 +19,12 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->count(100)->create();
 
-        User::query()->inRandomOrder()->limit(10)->get()->each(fn(User $u) => Project::factory()->create(['created_by' => $u->id]));
+        User::query()->inRandomOrder()->limit(10)->get()
+            ->each(function (User $u) {
+                $project = Project::factory()->create(['created_by' => $u->id]);
+
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+            });
 
         // Cria um usuário específico
         // User::factory()->create([
