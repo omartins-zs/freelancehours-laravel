64aeff39be26abee0b4fbc41e0c79a2a3d9d338d
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-09 21:44:57 -0300

    Ajustando Estrutura da Migration Proposals

--- database/migrations/2024_10_09_232827_create_proposals_table.php
+++ database/migrations/2024_10_09_232827_create_proposals_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,9 @@ return new class extends Migration
     {
         Schema::create('proposals', function (Blueprint $table) {
             $table->id();
+            $table->string('email');
+            $table->unsignedSmallInteger('hours');
+            $table->foreignIdFor(Project::class)->constrained();
             $table->timestamps();
         });
     }
