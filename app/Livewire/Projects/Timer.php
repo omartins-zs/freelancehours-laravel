a76e43a397e3dae4ed7a94a17b444f95b57c649a
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-11 12:23:06 -0300

    Criando Component Livewire para Timer dos Projetos

---
+++ app/Livewire/Projects/Timer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Projects;
+
+use Livewire\Component;
+
+class Timer extends Component
+{
+    public function render()
+    {
+        return view('livewire.projects.timer');
+    }
+}
