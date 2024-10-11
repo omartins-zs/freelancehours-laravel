588dd05c46e8e1ebc96260aa6fd41f6a37f44085
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-11 00:03:13 -0300

    Trazendo apenas o Component do projects card

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -1,11 +1,1 @@
-<div>
-
-    Component Livewire projects.show
-
-    <pre>
-       title: {{ $project->title }}
-       
-       {{-- !! Ele interpreta e já mostra corretamente o codgio em HTML --}}
-       description: {!! $project->description !!}
-    </pre>
-</div>
+<x-projects.card :$project />
