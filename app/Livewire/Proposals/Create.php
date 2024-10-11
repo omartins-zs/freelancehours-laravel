756e0b07edcce413bc179b613f6b64434548b31b
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-11 09:03:30 -0300

    Add Props email, hours e o $project para o Create

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -2,11 +2,17 @@
 
 namespace App\Livewire\Proposals;
 
+use App\Models\Project;
 use Livewire\Component;
 
 class Create extends Component
 {
+    public Project $project;
+
     public bool $modal = true;
+
+    public string $email;
+    public int $hours;
 
     public function render()
     {
