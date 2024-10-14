e40fedad52b4220ecfb509c92864d1e93fe2824e
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-14 13:35:44 -0300

    Ajuste no Pagination e Update no Proposal created

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -4,24 +4,32 @@ namespace App\Livewire\Projects;
 
 use App\Models\Project;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Proposals extends Component
 {
     public Project $project;
 
-    public int $qty = 10;
+    public int $qty = 5;
 
     #[Computed()]
     public function proposals()
     {
-        return $this->project->proposals()->orderByDesc('hours')->simplePaginate($this->qty);
+        return $this->project->proposals()->orderBy('hours')->simplePaginate($this->qty);
+    }
+
+    #[Computed()]
+    public function lastProposalTime()
+    {
+        return $this->project->proposals()->latest()->first()->created_at->diffForHumans();
     }
 
     public function loadMore()
     {
         $this->qty += 10;
     }
+    #[On('proposal::created')]
 
     public function render()
     {
