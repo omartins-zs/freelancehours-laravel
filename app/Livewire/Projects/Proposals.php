5585810e0c68c9b00a513eaa0b5a1168edd07d73
omartins-zs <gabrielmatheus209@gmail.com>
2024-10-14 11:17:13 -0300

    Add Qtde de Propostas por Projeto no Proposals

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,11 +3,20 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Proposals extends Component
 {
     public Project $project;
+
+    public int $qty = 10;
+
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->proposals()->orderByDesc('hours')->limit($this->qty)->get();
+    }
 
     public function render()
     {
